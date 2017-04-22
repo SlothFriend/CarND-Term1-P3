82a059594156b7e538ca310e164afaf69161c8b0
David Clark <davidclark314@gmail.com>
2017-04-21 20:07:06 -0600

    Updated model.py to match the altered file I had run on the AWS instance.

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import math
 import csv
 import cv2
 import numpy as np
@@ -9,11 +8,13 @@ from sklearn.model_selection import train_test_split
 def get_data():
     folders = [
         'forwards-lap-data',
-        # 'backwards-lap-data',
-        # 'corrective-data',
-        # 'corner-data',
-        # 'jungle-one-lap-data',
-        # 'jungle-backwards-lap-data',
+        'backwards-lap-data',
+        'corrective-data',
+        'corner-data',
+        'jungle-one-lap-data',
+        'jungle-backwards-lap-data',
+        'normal-lap-data',
+        'problem-spots-data',
     ]
 
     image_load_data = []
@@ -40,7 +41,7 @@ def get_data():
             angles.append(steering_angle)
             angles.append(-steering_angle)
 
-    return shuffle(np.array(images), np.array(angles))
+    return np.array(images), np.array(angles)
 
 
 data, labels = get_data()
@@ -49,27 +50,28 @@ from keras.models import Sequential
 from keras.layers import Conv2D, Cropping2D, Dense, Dropout, Flatten, Lambda, MaxPooling2D
 
 batch = 128
-epochs = 1
+epochs = 7
 activation_type = 'elu'
-dropout_p = .2
+dropout_p = .3
 
 model = Sequential()
 model.add(Cropping2D(cropping=((60, 25), (0, 0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: (x / 127.5) - 1))
+model.add(Lambda(lambda x: (x / 255) - .5))
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation=activation_type))
 # model.add(MaxPooling2D())
-# model.add(Dropout(dropout_p))
+model.add(Dropout(dropout_p))
 model.add(Conv2D(36, (5, 5), strides=(2, 2), activation=activation_type))
 # model.add(MaxPooling2D())
-# model.add(Dropout(dropout_p))
+model.add(Dropout(dropout_p))
 model.add(Conv2D(48, (5, 5), strides=(2, 2), activation=activation_type))
 # model.add(MaxPooling2D())
-# model.add(Dropout(dropout_p))
+model.add(Dropout(dropout_p))
 model.add(Conv2D(64, (3, 3), activation=activation_type))
 # model.add(MaxPooling2D())
-# model.add(Dropout(dropout_p))
+model.add(Dropout(dropout_p))
 model.add(Conv2D(64, (3, 3), activation=activation_type))
 # model.add(MaxPooling2D())
+model.add(Dropout(dropout_p))
 model.add(Flatten())
 model.add(Dense(1162, activation=activation_type))
 model.add(Dense(100, activation=activation_type))
@@ -83,4 +85,4 @@ model.compile(optimizer='adam', loss='mse')
 model.fit(data, labels, batch_size=batch, epochs=epochs, validation_split=.2)
 
 print('Saved the model')
-model.save('model.k')
+model.save('model.h5')
